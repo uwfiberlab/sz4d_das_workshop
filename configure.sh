7aafd12dbf55ba762125fd13b73cd5236406f71b
Yiyu Ni <niyiyu@uw.edu>
2025-12-10 15:07:13 -0800

    Update conda and pip install commands in configure.sh
    Removed gmt from conda environment and pip install commands.

--- configure.sh
+++ configure.sh
@@ -4,9 +4,9 @@ git clone --depth=1 https://github.com/uwfiberlab/sz4d_das_workshop.git
 cd sz4d_das_workshop
 
 source /srv/conda/etc/profile.d/conda.sh
-conda create --name workshop python=3.10 h5py scipy numpy pandas matplotlib pytorch-cpu gmt -y
+conda create --name workshop python=3.10 h5py scipy numpy pandas matplotlib pytorch-cpu -y
 conda activate workshop
-pip install noisepy-seis ipykernel tqdm ELEP joblib seisbench pyocto pygmt
+pip install noisepy-seis ipykernel tqdm ELEP joblib seisbench
 python -m ipykernel install --user --name=workshop
 
 # copying DAS data from our S3 bucket. Should take about 2-4 minutes to complete. 
