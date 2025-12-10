a66bd6bd0f933ef2f3792c2fe57637af89dff8d4
Yiyu Ni <niyiyu@uw.edu>
2025-12-10 14:21:40 -0800

    Update conda environment and install additional packages

--- configure.sh
+++ configure.sh
@@ -3,10 +3,11 @@
 git clone --depth=1 https://github.com/uwfiberlab/sz4d_das_workshop.git
 cd sz4d_das_workshop
 
-conda create --name workshop python=3.10 h5py scipy numpy pandas matplotlib pytorch-cpu -y
+source /srv/conda/etc/profile.d/conda.sh
+conda create --name workshop python=3.10 h5py scipy numpy pandas matplotlib pytorch-cpu gmt -y
 conda activate workshop
-pip install noisepy-seis ipykernel tqdm ELEP joblib seisbench
+pip install noisepy-seis ipykernel tqdm ELEP joblib seisbench pyocto pygmt
 python -m ipykernel install --user --name=workshop
 
-# copying DAS data from our S3 bucket. Should take about 2-4 minutes to complete.
+# copying DAS data from our S3 bucket. Should take about 2-4 minutes to complete. 
 aws s3 cp --recursive --no-sign-request s3://2025-sz4d-das-workshop/ ./
